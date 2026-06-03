1d2b4c0999f050dc51c41d8a96097a9890f5c2f1
Lil Thalis <jacomossi.thalison@escola.pr.gov.br>
2026-06-03 07:53:51 -0300

    INDEX

---
+++ script.js
@@ -0,0 +1,21 @@
+const links = document.querySelectorAll("a");
+
+links.forEach(link => {
+
+link.addEventListener("click", function(e){
+
+if(this.hash !== ""){
+
+e.preventDefault();
+
+const hash = this.hash;
+
+document.querySelector(hash).scrollIntoView({
+behavior:"smooth"
+});
+
+}
+
+});
+
+});
