ffb5249c14a3e4e7cb8b3bd000324570fdd75cd3
Lil Thalis <jacomossi.thalison@escola.pr.gov.br>
2026-06-03 08:12:05 -0300

    index

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
-const links = document.querySelectorAll("a");
+// Navbar muda ao rolar
 
-links.forEach(link => {
+window.addEventListener("scroll", () => {
 
-link.addEventListener("click", function(e){
+    const navbar =
+    document.querySelector(".navbar");
 
-if(this.hash !== ""){
+    if(window.scrollY > 100){
 
-e.preventDefault();
+        navbar.style.background =
+        "rgba(0,0,0,.95)";
 
-const hash = this.hash;
+    }else{
 
-document.querySelector(hash).scrollIntoView({
-behavior:"smooth"
+        navbar.style.background =
+        "rgba(0,0,0,.7)";
+    }
 });
 
-}
+// Scroll Reveal simples
+
+const cards =
+document.querySelectorAll(
+".menu-card, .feature"
+);
+
+const observer =
+new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.style.opacity = "1";
+
+            entry.target.style.transform =
+            "translateY(0)";
+        }
+
+    });
 
 });
+
+cards.forEach(card => {
+
+    card.style.opacity = "0";
+
+    card.style.transform =
+    "translateY(40px)";
+
+    card.style.transition =
+    ".8s ease";
+
+    observer.observe(card);
 
 });
